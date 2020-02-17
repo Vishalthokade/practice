fc720e99e17a9faf12b482c262cf0eba903cdc0b
Vishal Thokade <vishalthokade1996@gmail.com>
2020-02-17 17:43:41 +0530

    Create strlen simulation.c

---
+++ strlen simulation.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{ 
+    //simulation of strconcat
+    char src[100]=pune;
+    char dest[100]=mh;
+    
+    while(dest[]='\0'){
+        
+    }
+    printf("concated string: %s", dest);
+    return 0;
+}
